d4f3d95d4df94ebebed47f62d59064bdbbb05685
Sonu Alam <khanthedominator007@gmail.com>
2026-01-25 18:13:55 +0545

    minor issues

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,12 @@
 
-
-
-
-
 import Hero1 from "@/components/Hero-Comp/Hero1"; 
 import Hero2 from "@/components/Hero-Comp/Hero2"; 
 import DestinationsCluster from "@/components/DestinationsCluster";
-import Hero3 from "@/componentS/Hero-Comp/Hero3"; 
-
+import Hero3 from "@/components/Hero-Comp/Hero3"; 
+import ServicesEditorial from "@/components/ServicesEditorial";
 import Founder from "@/components/Founder"; 
 import Stats from "@/components/Stats";
 import ContactSection from "@/components/Contact";
-
-import ServicesEditorial from "@/components/ServicesEditorial";
-
-
 import TestimonialsCarousel from "@/components/Testimonials";
 
 export default function Home() {
@@ -24,23 +16,14 @@ export default function Home() {
       <Hero2/> 
       <DestinationsCluster/>  
       <Hero3/>
-
-
-     
-    
-     
+ 
       <ServicesEditorial/>
       <Founder/>  
-      <Stats/>  
-     
-      
+      <Stats/> 
+
       <TestimonialsCarousel/>  
       <ContactSection/> 
-  
-
-   
      
-      
-    </>
+   </>
   );
 }
